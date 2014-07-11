00b9a14d754fdb3dd88c787339f67afcd2734225
Martin Halamíček <martin@halamicek.cz>
2014-07-11 10:06:55 +0200

    PHP54 fixes

--- recipes/php.rb
+++ recipes/php.rb
@@ -2,11 +2,24 @@
 
 include_recipe "php"
 
-template "#{node['php']['ext_conf_dir']}/opcache.ini" do
+php_pear "zendopcache" do
+  action :install
+  version "7.0.3"
+end
+
+# We install the PHP packages at compile time in order to have the php-config executable available for query
+ext_dir = `php-config --extension-dir`.chomp
+ext_dir.empty? && raise('Could not execute php-config to locate the PHP extension dir. Please install the PHP development libraries')
+Chef::Log.info("Discovered PHP extension dir to be #{ext_dir}")
+
+template "#{node['php']['ext_conf_dir']}/zendopcache.ini" do
   source 'opcache.ini.erb'
   owner 'root'
   group 'root'
   mode 00644
+  variables({
+      :ext_dir => ext_dir
+    })
   if node['recipes'].include?('php::fpm')
     notifies :restart, "service[#{svc}]"
   end
