fc6ecc44a4fba872a3d90d2239fada444c098911
Martin Halamíček <martin@halamicek.cz>
2014-07-11 10:43:40 +0200

    fix: opcache path

--- recipes/php.rb
+++ recipes/php.rb
@@ -7,10 +7,6 @@ php_pear "zendopcache" do
   version "7.0.3"
 end
 
-# We install the PHP packages at compile time in order to have the php-config executable available for query
-ext_dir = `php-config --extension-dir`.chomp
-ext_dir.empty? && raise('Could not execute php-config to locate the PHP extension dir. Please install the PHP development libraries')
-Chef::Log.info("Discovered PHP extension dir to be #{ext_dir}")
 
 template "#{node['php']['ext_conf_dir']}/zendopcache.ini" do
   source 'opcache.ini.erb'
@@ -18,7 +14,7 @@ template "#{node['php']['ext_conf_dir']}/zendopcache.ini" do
   group 'root'
   mode 00644
   variables({
-      :ext_dir => ext_dir
+      :ext_dir => '/usr/lib64/php/modules'
     })
   if node['recipes'].include?('php::fpm')
     notifies :restart, "service[#{svc}]"
