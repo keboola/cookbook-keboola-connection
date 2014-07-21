97b22b0185c9767251a7c98bbc27594dd9899f7b
Martin Halamíček <martin@halamicek.cz>
2014-07-21 15:18:39 +0200

    fix: newrelic application monitoring

--- attributes/default.rb
+++ attributes/default.rb
@@ -29,3 +29,5 @@ default['nodejs']['checksum_linux_x64'] = '305bf2983c65edea6dd2c9f3669b956251af0
 default['nodejs']['checksum_linux_x86'] = '8fa2d952556c8b5aa37c077e2735c972c522510facaa4df76d4244be88f4dc0f'
 default['nodejs']['checksum_linux_arm-pi'] = '561ec2ebfe963be8d6129f82a7d1bc112fb8fbfc0a1323ebe38ef55850f25517'
 default['nodejs']['dir'] = '/usr'
+
+default['newrelic']['php-agent']['config_file'] = "/etc/php.d/newrelic.ini"
