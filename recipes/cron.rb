13819a71d673df28d5b2f550512dcd498cc4203e
Martin Halamíček <martin@halamicek.cz>
2015-02-02 10:05:20 +0100

    removed old twitter extractor cron jobs

--- recipes/cron.rb
+++ recipes/cron.rb
@@ -8,37 +8,6 @@ cron "token expirator" do
   action action
 end
 
-cron "twitter import-export" do
-  user "deploy"
-  hour "09"
-  minute "02"
-  command "/www/connection/current/cli.sh twitter:cron:import-export --lock >/dev/null 2>&1"
-  action action
-end
-
-cron "twitter update-engaged-users" do
-  user "deploy"
-  hour "07"
-  minute "02"
-  command "/www/connection/current/cli.sh twitter:cron:update-engaged-users --lock >/dev/null 2>&1"
-  action action
-end
-
-cron "twitter import-retweets-of-user" do
-  user "deploy"
-  minute "17"
-  command "/www/connection/current/cli.sh twitter:cron:import-retweets-of-user --lock >/dev/null 2>&1"
-  action action
-end
-
-cron "twitter import-source-tweets" do
-  user "deploy"
-  minute "*/5"
-  command "/www/connection/current/cli.sh twitter:cron:import-source-tweets --lock >/dev/null 2>&1"
-  action action
-end
-
-
 cron "elastic events roll index" do
   user "deploy"
   hour "00"
