4272d0f5d82a5e328d4ecf453ea1c8b71772b578
Martin Halamíček <martin@halamicek.cz>
2015-06-11 15:15:28 +0200

    cron - logging fix

--- recipes/cron.rb
+++ recipes/cron.rb
@@ -4,13 +4,13 @@ action = node['keboola-connection']['enable_cron'].to_i > 0 ? :create : :delete
 
 cron "token expirator" do
   user "deploy"
-  command "/www/connection/current/cli.sh storage:token-expirator --lock >/dev/null 2>&1 | /usr/bin/logger -t 'token-expirator' -p local1.info"
+  command "/www/connection/current/cli.sh storage:token-expirator --lock 2>&1 | /usr/bin/logger -t 'token-expirator' -p local1.info"
   action action
 end
 
 cron "backend stats update" do
   user "deploy"
-  command "/www/connection/current/cli.sh storage:update-backend-stats --lock >/dev/null 2>&1 | /usr/bin/logger -t 'update-backend-stats' -p local1.info"
+  command "/www/connection/current/cli.sh storage:update-backend-stats --lock 2>&1 | /usr/bin/logger -t 'update-backend-stats' -p local1.info"
   minute "38"
   action action
 end
@@ -20,6 +20,6 @@ cron "elastic events roll index" do
   hour "00"
   minute "00"
   day "1"
-  command "/www/connection/current/cli.sh storage:elastic-events-roll-index --lock >/dev/null 2>&1 | /usr/bin/logger -t 'elastic-events-roll-index' -p local1.info"
+  command "/www/connection/current/cli.sh storage:elastic-events-roll-index --lock 2>&1 | /usr/bin/logger -t 'elastic-events-roll-index' -p local1.info"
   action action
 end
