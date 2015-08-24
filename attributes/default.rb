e5ce9480b040301829f43110bbb4caa7adfda813
Martin Halamíček <martin@halamicek.cz>
2015-08-24 17:29:42 +0200

    new relic php agent path

--- attributes/default.rb
+++ attributes/default.rb
@@ -14,4 +14,4 @@ default['aws']['aws_secret_access_key'] = ''
 default['php']['packages'] = %w{ php56 php56-opcache php56-devel php56-gd php-pear php56-pdo php56-mysqlnd php56-pgsql php56-mcrypt }
 
 
-default['newrelic']['php_agent']['config_file'] = "/etc/php.d/newrelic.ini"
+default['newrelic']['php_agent']['config_file'] = "/etc/php-5.6.d/newrelic.ini"
