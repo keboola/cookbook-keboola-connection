1f5f196b228a138c972a9cd4aaa4b67340737740
Martin Halamíček <martin@halamicek.cz>
2015-11-13 07:42:10 +0100

    use latest apache recipe, prefork settings moved to apache recipe

--- attributes/default.rb
+++ attributes/default.rb
@@ -16,10 +16,3 @@ default['php']['packages'] = %w{ php56 php56-opcache php56-devel php56-gd php-pe
 default['newrelic']['php_agent']['config_file'] = "/etc/php-5.6.d/newrelic.ini"
 
 default['sysctl']['params']['vm']['vfs_cache_pressure'] = 10000
-
-default['apache']['prefork']['startservers']        = 5
-default['apache']['prefork']['minspareservers']     = 5
-default['apache']['prefork']['maxspareservers']     = 10
-default['apache']['prefork']['serverlimit']         = 256
-default['apache']['prefork']['maxrequestworkers']   = 256
-default['apache']['prefork']['maxconnectionsperchild'] = 10_000
