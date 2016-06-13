061692f14dfcbf098a7be85628a7eed2bde8573d
Martin Halamíček <martin@halamicek.cz>
2016-06-13 14:14:56 +0200

    php - 512MB - snowflake sync export issue

--- attributes/default.rb
+++ attributes/default.rb
@@ -13,6 +13,12 @@ default['aws']['aws_secret_access_key'] = ''
 
 default['php']['packages'] = %w{ php56 php56-opcache php56-devel php56-gd php-pear php56-pdo php56-mysqlnd php56-pgsql php56-mcrypt }
 
+default['php']['directives'] = {
+    'date.timezone' => 'Europe/Prague',
+    'always_populate_raw_post_data' => '-1',
+    'memory_limit' => '512M'
+}
+
 default['newrelic']['php_agent']['config_file'] = "/etc/php-5.6.d/newrelic.ini"
 
 default['sysctl']['params']['vm']['vfs_cache_pressure'] = 10000
